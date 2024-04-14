bc3a16c1b880daed7f172834d86845118e17736b
JakaKupljen <jaka.kupljen@student.um.si>
2024-04-14 21:27:38 +0200

    test1

--- zapakiraj_27116.sh
+++ zapakiraj_27116.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# Pass secrets and tag as arguments or environment variables
-DOCKER_USERNAME=$1
-DOCKER_PASSWORD=$2
-REPO_NAME=$3
-TAG=$4
 
-# Log in to Docker using Docker login with a token
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
 
-# Build Docker image with the provided tag
 docker build . --file Dockerfile --tag $DOCKER_USERNAME/$REPO_NAME:$TAG
 
-# Push Docker image to DockerHub
 docker push $DOCKER_USERNAME/$REPO_NAME:$TAG
--- zazeni_27116.sh
+++ zazeni_27116.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-# Pass secrets and tag as arguments or environment variables
-DOCKER_USERNAME=$1
-DOCKER_PASSWORD=$2
-REPO_NAME=$3
-TAG=$4
 
-# Pull Docker image from DockerHub using the provided tag
+
 docker pull $DOCKER_USERNAME/$REPO_NAME:$TAG
 
-# Run Docker container
 docker run -d -p 8080:80 $DOCKER_USERNAME/$REPO_NAME:$TAG
