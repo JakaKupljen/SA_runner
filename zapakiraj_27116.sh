b1a3d4ecb70bb21e0f1ac25229e64433c011a19e
JakaKupljen <jaka.kupljen@student.um.si>
2024-04-14 17:14:15 +0200

    dockertest11

--- zapakiraj_27116.sh
+++ zapakiraj_27116.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
-# Pass secrets as arguments or environment variables
+# Pass secrets and tag as arguments or environment variables
 DOCKER_USERNAME=$1
 DOCKER_PASSWORD=$2
 REPO_NAME=$3
+TAG=$4
 
 # Log in to Docker using Docker login with a token
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
 
-# Construct the tag using the current date and time
-TAG="$(date +'%Y-%m-%d--%H-%M')"
-
-# Build Docker image with the constructed tag
+# Build Docker image with the provided tag
 docker build . --file Dockerfile --tag $DOCKER_USERNAME/$REPO_NAME:$TAG
 
 # Push Docker image to DockerHub
--- zazeni_27116.sh
+++ zazeni_27116.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Pull Docker image from DockerHub
+# Pass secrets and tag as arguments or environment variables
+DOCKER_USERNAME=$1
+DOCKER_PASSWORD=$2
+REPO_NAME=$3
+TAG=$4
+
+# Pull Docker image from DockerHub using the provided tag
 docker pull $DOCKER_USERNAME/$REPO_NAME:$TAG
 
 # Run Docker container
